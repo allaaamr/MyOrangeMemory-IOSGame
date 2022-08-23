61f89e7e33f6be209d897df6483338b81f5e9293
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2022-08-23 11:46:35 +0200

    Initial Commit

---
+++ YourOrangeMemory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YourOrangeMemory
+//
+//  Created by Alaa Amr Abdelazeem on 23/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YourOrangeMemory/YourOrangeMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourOrangeMemoryApp.swift
+//  YourOrangeMemory
+//
+//  Created by Alaa Amr Abdelazeem on 23/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct YourOrangeMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
